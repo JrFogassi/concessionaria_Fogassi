3c2e8629ac2b381e1f35d5fdbeb86cf5b8f4c3b1
JrFogassi <jrfogassi@gmail.com>
2026-07-16 17:20:37 -0300

    ✨feat: navbar header dropdown

--- js/navigation.js
+++ js/navigation.js
@@ -1,13 +1,20 @@
-const hamburguer = document.querySelector('.hamburguer');
-const navLinks = document.querySelector('.nav-links');
+const hamburguer = document.querySelector(".hamburguer");
+const navLinks = document.querySelector(".nav-links");
 let menuAberto = false;
 
-hanburguer.addEventListener('click', () => {
-    if (menuAberto == false){
-        navLinks.style.display = "block";
-        menuAberto = true;
-    } else if (menuAberto == true) {
-        navLinks.style.display = "none";
-        menuAberto = false;
-    }
+hamburguer.addEventListener("click", () => {
+  if (menuAberto == false) {
+    navLinks.classList.add("ativo");
+    menuAberto = true;
+  } else if (menuAberto == true) {
+    navLinks.classList.remove("ativo");
+    menuAberto = false;
+  }
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 768) {
+    navLinks.classList.remove("ativo");
+    menuAberto = false;
+  }
 });
